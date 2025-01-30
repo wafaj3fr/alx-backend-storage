2f2c2257a9b189afbeb16980a1028ab53d09c8b8
wafa <wafagaafar@gmail.com>
2025-01-30 03:35:43 -0800

    revised 10,11

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-"""
-Change school topics
-"""
+""" MongoDB Operations with Python using pymongo """
 
 
 def update_topics(mongo_collection, name, topics):
-    """
-    changes all topics of a school
-     document based on the name
-    """
-       mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    """ Changes all topics of a school document based on the name """
+    query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
+
+    mongo_collection.update_many(query, new_values)
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-"""
-Where can I learn Python?
-"""
+""" MongoDB Operations with Python using pymongo """
 
 
 def schools_by_topic(mongo_collection, topic):
-    """
-     returns the list of school having a specific topic
-    """
-     return mongo_collection.find({"topics": topic})
+    """ returns the list of school having a specific topic """
+    documents = mongo_collection.find({"topics": topic})
+    return list(documents)
