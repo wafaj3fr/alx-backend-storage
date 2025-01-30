b779d25982d5c5597d418c17d7ac449a50219d71
wafa <wafagaafar@gmail.com>
2025-01-30 03:33:37 -0800

    revised 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
-"""
-Insert a document in Python
-"""
+""" MongoDB Operations with Python using pymongo """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """
-     inserts a new document in a
-      collection based on kwargs
-      """
-       new_documents = mongo_collection.insert_one(kwargs)
-    return new_documents.inserted_id
+    """ Inserts a new document in a collection based on kwargs """
+    return mongo_collection.insert(kwargs)
